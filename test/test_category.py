c8448747167f9f795ca4b72309dfd2232a2eaf17
jenuk <doctor_who_1996@mail.ru>
2024-11-13 15:02:25 +0500

    fix_test

--- test/test_category.py
+++ test/test_category.py
@@ -4,14 +4,14 @@ from src.category import Category
 from src.products import Product
 
 
-new_product = Product.new_product(
-    {
-        "name": "Samsung Galaxy S23 Ultra",
-        "description": "256GB, Серый цвет, 200MP камера",
-        "price": 180000.0,
-        "quantity": 5,
-    }
-)
+#new_product = Product.new_product(
+#    {
+#        "name": "Samsung Galaxy S23 Ultra",
+#        "description": "256GB, Серый цвет, 200MP камера",
+#        "price": 180000.0,
+#        "quantity": 5,
+#    }
+#)
 
 
 @pytest.fixture
