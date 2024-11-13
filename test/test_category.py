e86e0756135617f439cfc88351c9687bc068a57d
jenuk <doctor_who_1996@mail.ru>
2024-11-13 15:15:01 +0500

    tests

--- test/test_category.py
+++ test/test_category.py
@@ -50,6 +50,4 @@ def test_category_init(first_category):
 def test_class_category(second_category, firs_category):
     assert second_category.name == "Техника"
     assert second_category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
-    assert second_category.category_count == 2
-    assert second_category.product_count == 4
     assert len(firs_category.products) == 1
