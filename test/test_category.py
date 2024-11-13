4db84fe668f96cf8d4a1e918599fc54a41ad3488
jenuk <doctor_who_1996@mail.ru>
2024-11-13 15:12:32 +0500

    workd

--- test/test_category.py
+++ test/test_category.py
@@ -1,23 +1,55 @@
 import pytest
 
 from src.category import Category
-from test.test_products import samsung, iphone, product1, product2
+from src.products import Product
 
-category1 = Category("Смартфоны",
-                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                         [product1, product2])
+new_product = Product.new_product(
+    {
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5,
+    }
+)
+
 
 @pytest.fixture
-def first_category():
-    return Category(name = "Смартфоны",
-                    description = "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                    products = [samsung, iphone]
+def first_category(samsung, iphone):
+    return Category(name="Смартфоны",
+                    description="Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+                    products=[samsung, iphone]
                     )
 
-def test_category_init(first_category, second_category):
+
+@pytest.fixture
+def samsung():
+    return Product(
+        name="Samsung Galaxy S23 Ultra",
+        description="256GB, Серый цвет, 200MP камера",
+        price=180000.0,
+        quantity=5
+    )
+
+
+@pytest.fixture
+def iphone():
+    return Product(
+        name="Iphone 15",
+        description="512GB, Gray space",
+        price=210000.0,
+        quantity=8
+    )
+
+
+def test_category_init(first_category):
     assert first_category.name == "Смартфоны"
     assert first_category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
-    assert len(first_category.list_product) == 3
+    assert len(first_category.products) == 2
 
-    assert first_category.category_count == 2
+
+def test_class_category(second_category, firs_category):
+    assert second_category.name == "Техника"
+    assert second_category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert second_category.category_count == 2
     assert second_category.product_count == 4
+    assert len(firs_category.products) == 1
