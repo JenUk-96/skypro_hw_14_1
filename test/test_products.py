b160861a5c50286c2a552461aa34796efca2ad86
jenuk <doctor_who_1996@mail.ru>
2024-11-26 01:08:02 +0500

    tests_new

--- test/test_products.py
+++ test/test_products.py
@@ -77,37 +77,16 @@ def test_add_smartphone(product_one, product_two):
     assert 198000.0
 
 
-class TestCaseProduct(unittest.TestCase):
+def test_repr(product_one):
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    assert repr(product1) == "Product(Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 180000.0, 5)"
 
-    def test_add(self):
-        smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
-                                 "S23 Ultra", 256, "Серый")
-        smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
-        grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
-        grass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
-        category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
-        with self.assertRaises(TypeError):
-            invalid_sum = smartphone1 + grass1
-            invalid_sum_2 = grass2 + smartphone2
-            invalid_sum_3 = grass2 + category_smartphones
 
-    def test_add_positive(self):
-        smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
-                                 "S23 Ultra", 256, "Серый")
-        smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
-        grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
-        grass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
-        assert grass1 + grass2 == 16750.0
-        assert smartphone1 + smartphone2 == 2580000.0
+def test_repr_2(product_two):
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    assert repr(product2) == "Product(Iphone 15, 512GB, Gray space, 210000.0, 8)"
 
-    def test_creation_str(self):
-        product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-        product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-        product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-        assert str(product1) == 'Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт'
-        assert str(product2) == 'Iphone 15, 210000.0 руб. Остаток: 8 шт'
-        assert str(product3) == 'Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт'
 
-    def test_repr(self):
-        product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-        assert repr(product1) == "Product(Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 180000.0, 5)"
+def test_repr_3(product_tree):
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    assert  repr(product3) == "Product(Xiaomi Redmi Note 11, 1024GB, Синий, 31000.0, 14)"
